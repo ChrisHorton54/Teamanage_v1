c684e72e79749c7dc2763a9650791ff29d97f452
ChrisHorton54 <chris.hortonis@hotmail.co.uk>
2015-02-11 00:04:34 +0000

    Signed-off-by: ChrisHorton54 <chris.hortonis@hotmail.co.uk>

--- js/capture-phonegap.js
+++ js/capture-phonegap.js
@@ -13,22 +13,6 @@ function onFail(message) {
 }
 
   function uploadFile(mediaFile) {
-    var imgData = getBase64Image(mediaFile);
-    localStorage.setItem("listImage", "data:image/png;base64," + imgData);
-      
-    var lists = JSON.parse(localStorage.getItem("lists"));
-      
-     for(var i = 0; i < lists.length; i++){
-        if(lists[i].name == localStorage.getItem("listName")){
-            lists[i].image = "data:image/png;base64," + imgData;
-        }
-    }
-    
-    localStorage.setItem("lists",JSON.stringify(lists));
-      
-      window.location = "list-item.html";
+    var imgData = mediaFile;
+    $(".player-image img").attr("src","data:image/png;base64," + imgData);
   }
-        
-function getBase64Image(img) {
-    return img.replace(/^data:image\/(png|jpg);base64,/, "");
-}
