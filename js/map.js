dee0deac95ed9c10c0e356a88f86d0773bc0a1cf
ChrisHorton54 <chris.hortonis@hotmail.co.uk>
2015-02-20 17:30:45 +0000

    Work on the Management Events 1.3

--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
-var id, target, request, responseObject, marker,mapOptions,map,infowindow, longitude, latitude, myLatLng,contentString;
-
-localStorage.setItem("markerLat","1");
-localStorage.setItem("markerLng","1");
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
 
 function loadMap(){
-    initializeMap();
+    
+    initialize();
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(displayAndWatch, locError);
     } else {
@@ -12,53 +12,48 @@ function loadMap(){
         alert("Your browser does not support the Geolocation API!");
     }
     
-    viewport();
 }
-
-function viewport(){ 
-    var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    var header = 40;
-    
-    var newHeight = height - header;
-    
-    document.getElementById("map").style.height = newHeight + "px";
-    document.getElementById("map").style.width = width + "px";
-}
-
-function initializeMap(){
+function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
     var mapOptions = {
-      zoom: 13,
-      center: new google.maps.LatLng(0, 0),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom:7,
+    center: chicago
     };
-    map = new google.maps.Map(document.getElementById('map'),
-        mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
 }
 
-function locError(error) {
-    // tell the user if the current position could not be located
-    alert("The current position could not be found! Please turn on your Location");
+function calcRoute() {
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+    });
 }
 
 function displayAndWatch(position) {
     // set current position
     setCurrentPosition(position);
-    
-    mockAjaxRequest(position);
 
     // watch position
     watchCurrentPosition();
 }
+
 function setCurrentPosition(pos) {
     currentPositionMarker = new google.maps.Marker({
         map: map,
         position: new google.maps.LatLng(
             pos.coords.latitude,
             pos.coords.longitude
-        ),
-        title: "Current Position",
-        icon: 'img/home_marker.png'
+        )
     });
     map.panTo(new google.maps.LatLng(
             pos.coords.latitude,
@@ -76,70 +71,10 @@ function setCurrentPosition(pos) {
         });
 }
 
-function setMarkerPosition(marker, position) {
-    marker.setPosition(
-        new google.maps.LatLng(
-            position.coords.latitude,
-            position.coords.longitude)
-    );
+function locError(error) {
+    // tell the user if the current position could not be located
+    alert("The current position could not be found! Please turn on your Location");
 }
 
-function mockAjaxRequest(position){
-    
-	 request = new XMLHttpRequest();
 
-	request.open('get', 'marker.txt',true);
-
-	request.onreadystatechange = function(){
-        
-		if(request.readyState === 4){
-             //alert(request.status);
-			if(request.status === 200 || request.status === 0){
-                //alert("Recieved");
-				responseObject = JSON.parse(request.responseText);
-
-				markerRecieved(position,responseObject);
-
-			} else {
-				console.log("Failed");
-			}
-		}
-	}
-	request.send(null);
-
-}
-
-function markerRecieved(position,data){    
-    
-    infowindow = new google.maps.InfoWindow();
-    
-    for(var i = 0; i < data.markers.length; i++){   
-        myLatlng = new google.maps.LatLng(data.markers[i].lat,data.markers[i].lng);
-        marker = new google.maps.Marker({
-            position: myLatlng,
-            title: data.markers[i].name,
-            map: map,
-            icon: 'img/store_marker.png'
-        });
-        
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() { 
-          contentString = '<img src="' + data.markers[i].img + '"style="width:100px"/><p>' + data.markers[i].name + '</p>';
-            
-          infowindow.setContent(contentString);
-          infowindow.open(map, marker);
-            localStorage.setItem("markerLat",roundToTwo(marker.position.k));
-            localStorage.setItem("markerLng",roundToTwo(marker.position.D));
-        }
-      })(marker, i));
-    }
-    
-    if (localStorage.getItem("markerLat") == position.latitude && localStorage.getItem("markerLng") == position.longitude) {
-    alert('Congratulations, you reached the target');
-  }
-}
-
-function roundToTwo(num) {    
-    return +(Math.round(num + "e+5")  + "e-5");
-}
-
+google.maps.event.addDomListener(window, 'load', initialize);
